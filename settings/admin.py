bfb6a203eceef3f138879704854fdc20fb9628d9
Imran Khan <impathan007@gmail.com>
2022-03-10 21:47:59 +0530

    fix

--- settings/admin.py
+++ settings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Location, Degree, Department, Designation, EmailCategory, EmailTemplate, Pipeline, PipelineField, PipelineStage
+my_modules = [Location, Degree, Department, Designation, EmailCategory, EmailTemplate, Pipeline, PipelineField, PipelineStage]
+
+admin.site.register(my_modules)
