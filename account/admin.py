5e17f45c286e22bd4fbba37bc5875a7d2024328b
Imran Khan <impathan007@gmail.com>
2022-03-19 21:06:32 +0530

    test

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Account, Company
+from .models import Account, Company, TokenEmailVerification
 # Register your models here.
 
-admin.site.register([Account, Company])
+admin.site.register([Account, TokenEmailVerification, Company])
